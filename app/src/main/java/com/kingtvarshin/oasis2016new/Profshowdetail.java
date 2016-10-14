621f26935d964cde5d246bf7be89b88e03da8e0e
king <kingtvarshin1@gmail.com>
2016-10-15 04:41:08 +0530

    collapsingtoolbaradded

--- app/src/main/java/com/kingtvarshin/oasis2016new/Profshowdetail.java
+++ app/src/main/java/com/kingtvarshin/oasis2016new/Profshowdetail.java
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by lenovo on 14-10-2016.
@@ -16,6 +17,7 @@ import android.support.v7.widget.Toolbar;
 public class Profshowdetail extends AppCompatActivity {
 
     private CollapsingToolbarLayout ctbl = null;
+    String myParam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +29,30 @@ public class Profshowdetail extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null)
+        {
+            myParam = extras.getString("name");
+        }
+        else
+        {
+            //..oops!
+        }
+
+
         ctbl = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
-        ctbl.setTitle("game");
+        ctbl.setTitle(myParam);
 
         dynamicToolbarColor();
 
         toolbarTextAppearnce();
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();//What to do on back clicked
+            }
+        });
 
     }
 
